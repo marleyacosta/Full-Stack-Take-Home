30908744a11bb95570062f8b4dfc5d7779853b6a
Maurely Acosta <macos101@fiu.edu>
2020-01-12 23:23:39 -0500

    adding finished crud operations, need to do ui

--- boilerplate/app.js
+++ boilerplate/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const employeeRoutes = require('./routes/employee');
 const app = express();
 const port = parseInt(process.env.PORT || '3000');
@@ -9,8 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/employees', employeeRoutes);
+app.use('/', employeeRoutes);
 
-// Fail over route
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Fail over route 
 app.use(function(req, res) {
     res.status(404).send('Not found');
 });
--- boilerplate/routes/employee.js
+++ boilerplate/routes/employee.js
@@ -12,60 +12,122 @@ var shortid = require("shortid");
 
 var request = require('request');
 
+var bodyParser = require('body-parser');
+
 /* GET employees listing. */
 router.get('', function(req, res) {
 
-  return res.send(employees.find());
+  res.render("../views/employee/index", {employees: employees});
+
+  //return res.send(employees.find());
 });
 
 /* Create a new record using a randomly generated unique identifier */
 router.post('', function(req, res) {
 
-  var id = shortid.generate();
-  var fName = req.query.firstName;
-  var lName = req.query.lastName;
-  var hDate = req.query.hireDate;
-  var position = req.query.role;
+  var employeeDict = {};
+
+  if(req.query.role.toUpperCase() === 'ceo'.toUpperCase()){
+
+    if(employees.find({"role": { '$in' : [req.query.role.toUpperCase(), req.query.role.toLowerCase()]}})){
+      console.log("Error: There can only be one CEO");
+      return res.send(employees.find());
+    }
+
+  }
+
+  if(new Date(req.query.hireDate) >= new Date(Date.now())){
+
+    console.log("Error: The date must be in the past");
+      return res.send(employees.find());
+  }
+
+  employeeDict["identifier"] = shortid.generate();
+  employeeDict["firstName"] = req.query.firstName;
+  employeeDict["lastName"] = req.query.lastName;
+  employeeDict["hireDate"] = req.query.hireDate;
+  employeeDict["role"] = req.query.role;
+
+  
+
+  let jokeOptions = {
+    url: 'https://icanhazdadjoke.com/', 
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json', 
+        
+    },
+    json: true
+}
 
 
-  var joke = request('https://icanhazdadjoke.com', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    return (body.explanation);
+  var joke = request(jokeOptions, function(error, response, body){
+   if (!error && response.statusCode === 200) {
+        console.log(body.joke);
+        employeeDict["favoriteJoke"] = body.joke;
+        
+        
+    }
+    else{
+      console.log(error);
 
-                    });
+    }
+});
 
-  var quote = request('https://ron-swanson-quotes.herokuapp.com/v2/quotes', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    return (body.explanation);
-                    });
+                    
+let quoteOptions = {
+  url: 'https://ron-swanson-quotes.herokuapp.com/v2/quotes', 
+  method: 'GET',
+  headers: {
+      'Accept': 'application/json', 
+      
+  }
+}
+var quote = request(quoteOptions, function(error, response, body){
+  if (error) {
+      console.log(error);
+  } else if (!error && response.statusCode === 200) {
+      employeeDict["favoriteQuote"] = JSON.parse(body)[0];
+      
+  }
+});
 
 
-  employees.insert({ identifier: id, firstName : fName, lastName: lName, hireDate: hDate,
-     role: position, favoriteJoke: joke, favoriteQuote: quote});
+  employees.insert(employeeDict);
 
+     
   return res.send(employees.find());
 });
 
 /* Return the record corresponding to the id parameter */
-router.get(':id', function(req, res) {
+router.get('/:id', function(req, res) {
   var id = req.params.id;
   return res.send(employees.find({"identifier": id}));
 });
 
 /*Replace the record corresponding to :id with the contents of the PUT body*/
-router.put(':id', function(req, res) {
-
-  var originalEmployee = employees.find({'identifier': req.params.id});
+router.put('/:id', function(req, res) {
+  var id = req.params.id;
+  var originalEmployee = employees.findObject({'identifier': id});
   //Parse PUT body and replace values in the original employee
+  var replaceEmployee = req.body;
 
+  for (var attribute in replaceEmployee){
+    console.log(attribute+": "+replaceEmployee[attribute]);
+
+    if (replaceEmployee.hasOwnProperty(attribute) && originalEmployee.hasOwnProperty(attribute)) {
+      originalEmployee[attribute] = replaceEmployee[attribute];
+  }
+
+  }
   employees.update(originalEmployee);
-  return res.send(employees.find());
+  return res.send(employees.find({"identifier": id}));
 });
 
 /*delete the record corresponding to the id parameter*/
-router.delete(':id', function(req, res) {
+router.delete('/:id', function(req, res) {
 
-  return res.send(employees.find({'identifier': req.params.id}).remove());
+  return res.send(employees.findAndRemove({'identifier': req.params.id}));
 });
 
 module.exports = router;
